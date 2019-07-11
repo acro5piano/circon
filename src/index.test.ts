681557d492081f27d361c20a61864b99057cadfd
gosho-kazuya <ketsume0211@gmail.com>
2019-07-11 19:44:20 +0900

    initial feature pass

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,18 +1,56 @@
-import { run } from '.'
-// prettier-ignore
+import config from '.'
+const yaml = require('js-yaml')
 
-test('hoge', () => {
-  expect(run()).toEqual({})
+test('dump', () => {
+  config.docker('circleci/node:10.3.0', {
+    environment: {
+      TZ: '/usr/share/zoneinfo/Asia/Tokyo',
+    },
+  })
+
+  // prettier-ignore
+  config
+    .define('deploy')
+    .usePackage('yarn')
+    .branch('develop', 'master')
+    .tasks`
+      yarn tsc
+      yarn lint
+      yarn deploy
+    `
+
+  expect(config.toConfig()).toEqual(yaml.safeLoad`
+    version: 2
+    jobs:
+      deploy:
+        docker:
+          - image: 'circleci/node:10.3.0'
+            environment:
+              TZ: /usr/share/zoneinfo/Asia/Tokyo
+        working_directory: ~/repo
+        steps:
+          - checkout
+          - restore_cache:
+              keys:
+                - 'v2-dependencies-{{ checksum "yarn.lock" }}'
+                - v2-dependencies-
+          - run: yarn install
+          - save_cache:
+              paths:
+                - node_modules
+              key: 'v2-dependencies-{{ checksum "yarn.lock" }}'
+          - run: yarn tsc
+          - run: yarn lint
+          - run: yarn deploy
+    workflows:
+      version: 2
+      master_jobs:
+        jobs:
+          - deploy:
+              filters:
+                branches:
+                  only:
+                    - develop
+                    - master
+      `)
 })
-
-// job
-//   .define('deploy_payment_web')
-//   .tasks`
-//     yarn apollo-codegen:production
-//     yarn tsc
-//     yarn lint
-//     cp packages/nupp1-payment-api/{.env.ci,.env}
-//     yarn workspace @nupp1/payment-api knex migrate:latest
-//     yarn test:all:serial --coverage
-//   `
-//   .branch('develop', 'master')
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,135 @@
+const yaml = require('js-yaml')
+
 export function run() {
   return {}
 }
+
+interface Docker {
+  image: string
+  config?: object
+}
+
+type PackageManager = 'yarn' | 'bundler' | 'npm'
+
+class Job {
+  package: PackageManager | null = null
+  dockers: Docker[] = []
+  name = ''
+  branches: string[] = []
+  tasksLiteral!: TemplateStringsArray
+
+  docker(image: string, config: object | undefined) {
+    this.dockers.push({
+      image,
+      ...config,
+    })
+    return this
+  }
+
+  define(name: string) {
+    this.name = name
+    return this
+  }
+
+  branch(...branches: string[]) {
+    this.branches = branches
+    return this
+  }
+
+  tasks(...literal: TemplateStringsArray[]) {
+    this.tasksLiteral = literal[0]
+    return this
+  }
+
+  usePackage(pm: PackageManager) {
+    this.package = pm
+    return this
+  }
+
+  toPackageManagerCommandsWithCache() {
+    if (!this.package) {
+      return []
+    }
+    switch (this.package) {
+      case 'yarn':
+        return [
+          {
+            restore_cache: {
+              keys: ['v2-dependencies-{{ checksum "yarn.lock" }}', 'v2-dependencies-'],
+            },
+          },
+          { run: 'yarn install' },
+          {
+            save_cache: {
+              paths: ['node_modules'],
+              key: 'v2-dependencies-{{ checksum "yarn.lock" }}',
+            },
+          },
+        ]
+      case 'npm':
+        return [
+          {
+            restore_cache: {
+              keys: ['v2-dependencies-{{ checksum "package-lock.json" }}', 'v2-dependencies-'],
+            },
+          },
+          { run: 'npm install' },
+          {
+            save_cache: {
+              paths: ['node_modules'],
+              key: 'v2-dependencies-{{ checksum "package-lock.json" }}',
+            },
+          },
+        ]
+    }
+    return []
+  }
+
+  toConfig() {
+    return {
+      version: 2,
+      jobs: {
+        deploy: {
+          docker: this.dockers,
+          working_directory: '~/repo',
+          steps: [
+            'checkout',
+            ...this.toPackageManagerCommandsWithCache(),
+            ...this.tasksLiteral
+              .toString()
+              .split('\n')
+              .filter(t => Boolean(t.match(/[a-z]/)))
+              .map(t => ({
+                run: t.trim(),
+              })),
+          ],
+        },
+      },
+      workflows: {
+        version: 2,
+        master_jobs: {
+          jobs: [
+            {
+              [this.name]: {
+                filters: {
+                  branches: {
+                    only: this.branches,
+                  },
+                },
+              },
+            },
+          ],
+        },
+      },
+    }
+  }
+
+  dump() {
+    return yaml.safeDump(this.toConfig(), {
+      noRefs: true,
+      noCompatMode: true,
+    })
+  }
+}
+
+export default new Job()
